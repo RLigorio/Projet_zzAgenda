e7e29c91db2ed60ffd66e4bae28e760857cd2f26
Raphael LIGORIO <raligorio@etud.rcisima.isima.fr>
2017-11-20 19:33:42 +0100

    ajout evenements + minor bug fix

--- event.php
+++ event.php
@@ -1,11 +1,20 @@
 <?php
+	ini_set('display_errors', 1);
 	session_start();
 
 	$eventN = $_POST['EventName'];
 	$eventD = $_POST['EventDes'];
 	$eventL = $_POST['EventPlace'];
 	$eventS = $_POST['EventSpeak'];
-	$eventlist = file('event.txt');
+	$eventDa = $_POST['EventDate'];
+	$eventT = $_POST['EventTime'];
+	
+	$arr = json_decode(file_get_contents('event.json'), true);
+	
+	$arr['events'][] = array('title' => $eventN, 'description' => $eventD, 'location' => $eventL, 'speaker'=> $eventS, 'date' => $eventDa, 'time' => $eventT);
+
+	$json = json_encode($arr);
+	file_put_contents('event.json', $json);
 	
 	header('location: user.php');
 	
